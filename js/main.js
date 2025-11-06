3f3760182a8afacf64cce28f77d870d7752a87bb
erschm240 <schmitze608@gmail.com>
2025-11-06 11:19:42 -0700

    Continued work on loading watchlist and ratings dynamically on homepage.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 import "../css/style.css";
+import { watchlistTemplate, ratingsTemplate } from "./templates.mjs";
+
+function setwatchlistSection() {
+    const watchlistSection = document.querySelector("#watchlist");
+    watchlistSection.innerHTML = watchlistTemplate();
+}
+
+function setwatchlistSection() {
+    const watchlistSection = document.querySelector("#ratings");
+    watchlistSection.innerHTML = watchlistTemplate();
+}
---
+++ js/templates.mjs
@@ -0,0 +1,40 @@
+export function watchlistTemplate() {
+    return `<section id="watchlist">
+                <h2>Watchlist</h2>
+                <div class="movie-container">
+                    <img src="images/temporary_thumbnail.webp" alt="A thumbnail of the rated movie.">
+                    <div class="movie-info">
+                        <h3>Awesome Movie Title</h3>
+                        <p>A description about the movie.</p>
+                    </div>
+                </div>
+                <div class="movie-container">
+                    <img src="images/temporary_thumbnail.webp" alt="A thumbnail of the rated movie.">
+                    <div class="movie-info">
+                        <h3>Awesome Movie Title</h3>
+                        <p>A description about the movie.</p>
+                    </div>
+                </div>
+            </section>
+            `;
+}
+
+export function ratingsTemplate() {
+    return `<section id="ratings">
+                <h2>Ratings</h2>
+                <div class="movie-container">
+                    <img src="images/temporary_thumbnail.webp" alt="A thumbnail of the rated movie.">
+                    <div class="movie-info">
+                        <h3>Awesome Movie Title</h3>
+                        <p>★★★★★</p>
+                    </div>
+                </div>
+                <div class="movie-container">
+                    <img src="images/temporary_thumbnail.webp" alt="A thumbnail of the rated movie.">
+                    <div class="movie-info">
+                        <h3>Awesome Movie Title</h3>
+                        <p>★★★★★</p>
+                    </div>
+                </div>
+            </section>`;
+}
