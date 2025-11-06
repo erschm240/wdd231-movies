678689b8346dc27d9dc18cffa08a4c093929deff
erschm240 <schmitze608@gmail.com>
2025-11-06 10:42:17 -0700

    Added js files for dynamically loading content and getting api info.

---
+++ js/setHeaderFooter.mjs
@@ -0,0 +1,17 @@
+function headerTemplate() {
+    return `<a href="index.html"><img class="logo" src="images/temporary_icon.webp"></a>
+            <h1>Animated Movies</h1>
+            <button class="menu-btn">Menu</button>
+            <nav class="menu-nav">
+                <a href="index.html">Home</a>
+                <a href="search.html">Search</a>
+            </nav>`;
+}
+
+export function setHeader() {
+
+}
+
+function footerTemplate() {
+
+}
