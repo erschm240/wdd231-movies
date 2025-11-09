2c6a63aabb5b50b5ffc4988e648af61ff5fa6f06
lucedewitt <lucedewitt@byui.edu>
2025-11-08 21:35:52 -0700

    .env stuff

--- src/js/movies.mjs
+++ src/js/movies.mjs
@@ -1,8 +1,8 @@
 // This is where we will get the API data
 const baseUrl = "https://www.omdbapi.com/";
 
-// const apiKey = import.meta.env.VITE_OMDB_API_KEY;
-const apiKey = "616f22d1";
+const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+// const apiKey = "616f22d1";
 console.log(apiKey);
 
 async function getJson(queryParams) {
